5387ab0d08c0cacb0b541ad470e24e124177a225
Sanjioy <127838002+Sanjioy@users.noreply.github.com>
2024-06-01 11:45:22 +0300

    Initial commit

---
+++ app/src/main/java/com/stingach/dm/transport/Transport.kt
@@ -0,0 +1,8 @@
+package com.stingach.dm.transport
+
+data class Transport(
+    val name: String, // Название
+    val type: String, // "Мотоцикл" или "Автомобиль"
+    val loadCapacity: Int, // Грузоподъемность
+    val axleCount: Int // Количество осей
+)
